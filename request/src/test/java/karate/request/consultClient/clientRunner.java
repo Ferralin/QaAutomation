4abcc88a53fb08a5ef80abf717f9121f14cab303
Francisco Ferral <Franciscoferral536@gmail.com>
2022-08-22 11:26:42 -0500

    Remove create and refactoring

--- request/src/test/java/karate/request/consultClient/clientRunner.java
+++
@@ -1,9 +0,0 @@
-package karate.request.consultClient;
-
-import com.intuit.karate.junit5.Karate;
-public class clientRunner {
-    @Karate.Test
-        Karate testSample(){
-        return Karate.run("classpath:karate/request/consultClient/client.feature").relativeTo(getClass());
-    }
-}
